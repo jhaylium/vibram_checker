0b3aa1ad11be68e1ac4eaf6a92fd1d56edde9db6
Jeff <jmccain@haylium.com>
2022-08-25 23:13:23 -0400

    updated to include error message and send to text

--- main_p.py
+++ main_p.py
@@ -12,31 +12,40 @@ account_sid = os.environ['account_sid']
 auth_token = os.environ['token']
 client = Client(account_sid, auth_token)
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    url_size_44 = r'https://us.vibram.com/shop/shop-all-products/mens-fivefingers-1/#prefn1=size&prefv1=44'
-    page.goto(url_size_44)
-    soup = BeautifulSoup(page.inner_html('[id="main"]'), 'html.parser')
-    product_selector = soup.find_all('div', class_='product-tile')
-    products = []
-    for product in product_selector:
-        price = product.find('span', class_='product-sales-price').get_text()
-        name = product.find('a', class_='name-link')
-        price_var = float(price[1:])
-        name_var = name.get_text().strip().replace("\n", "").replace("\t", "")
-        url_var = name['href']
-        shoe_data = [name_var, price_var, url_var]
-        body = f"\n Product Name: {name_var} \n\n Price: ${price_var} \n\n Url: {url_var}"
-        if price_var < 90.0:
-            message = client.messages.create(
-                body=body,
-                from_='+18438912432',
-                to='+13055281278'
-            )
+try:
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        # url_size_44 = r'https://us.vibram.com/shop/shop-all-products/mens-fivefingers-1/#prefn1=size&prefv1=44'
+        url_size_44 = r'https://us.googleoop.com/'
+        page.goto(url_size_44)
+        soup = BeautifulSoup(page.inner_html('[id="main"]'), 'html.parser')
+        product_selector = soup.find_all('div', class_='product-tile')
+        products = []
+        for product in product_selector:
+            price = product.find('span', class_='product-sales-price').get_text()
+            name = product.find('a', class_='name-link')
+            price_var = float(price[1:])
+            name_var = name.get_text().strip().replace("\n", "").replace("\t", "")
+            url_var = name['href']
+            shoe_data = [name_var, price_var, url_var]
+            body = f"\n Product Name: {name_var} \n\n Price: ${price_var} \n\n Url: {url_var}"
+            if price_var < 90.0:
+                message = client.messages.create(
+                    body=body,
+                    from_=os.environ['from_number'],
+                    to=os.environ['to_number']
+                )
 
-        print(body)
-    browser.close()
+            print(body)
+        browser.close()
+except Exception as err:
+    body = str(err)[:1559]
+    message = client.messages.create(
+        body=err,
+        from_=os.environ['from_number'],
+        to=os.environ['to_number']
+    )
 
 def get_user_agent(num):
     agent = {
